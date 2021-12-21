ac1000fa98a5ceba770f1ffde576cc722be7a38a
Dan Tovbein <dtovbein@gmail.com>
2021-12-21 12:47:04 -0300

    Modify Home. Remove unused code.

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,49 +1,19 @@
-import React, { lazy, useMemo, memo, Suspense, useEffect, useState } from 'react';
-import { View, Wrapper } from '@bit/meema.ui-components.elements';
-import Carousel from '@bit/meema.ui-components.carousel';
+import React, { useMemo, memo, Suspense } from 'react';
+import Elements from '@bit/meema.ui-components.elements';
 import { HomeProvider } from './context';
 import { css } from 'styled-components';
 import { isMobile, pixelToRem } from 'meema.utils';
 import { Loader } from '../Shared';
 import Layout from '../Shared/Layout';
 import ErrorBoundary from '../ErrorBoundary';
-import Card from '../Card';
-import VideoPlayer from '../VideoPlayer';
-import UserDataForm from '../Forms/UserDataForm';
-import { useLocation } from 'react-router-dom';
 import MainHeader from '../Header';
+import ContentSlider from '../ContentSlider';
 
-type PathType = {
-  path: string;
-  index: number;
-}
-
-// const FormRouter = lazy(() => import('../Forms/router'));
 const mobile = isMobile();
-const paths: Array<PathType> = [
-  { path: '/user/information', index: 0 },
-  { path: '/video', index: 1 },
-];
 
 const Component: React.FunctionComponent<{}> = memo(() => {
-  const [ currentIndex, setCurrentIndex ] = useState(0);
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    const path = paths.filter((_: PathType) => _.path === pathname);
-    if(path.length) {
-      const timeout = setTimeout(() => {
-        setCurrentIndex(path[0].index);
-      }, 500) 
-
-      return () => {
-        clearTimeout(timeout);
-      }
-    }
-  }, [ pathname ]);
-
   return useMemo(() => (
-    <View
+    <Elements.View
       id='home'
       customCss={css`
         display: flex;
@@ -54,6 +24,7 @@ const Component: React.FunctionComponent<{}> = memo(() => {
         background-repeat: no-repeat;
         background-size: cover;
         background-position: center;
+        min-height: calc(100vh - ${({ theme }) => pixelToRem(theme.footer.tablet.height)});
         
         @media (min-width: ${({ theme }) => pixelToRem(theme.responsive.tablet.minWidth)}) {
           padding: 0;
@@ -64,7 +35,7 @@ const Component: React.FunctionComponent<{}> = memo(() => {
       <ErrorBoundary fallback='Header error'>
         <Suspense 
           fallback={
-            <Wrapper
+            <Elements.Wrapper
               customCss={css`
                 display: flex;
                 flex-direction: column;
@@ -82,13 +53,13 @@ const Component: React.FunctionComponent<{}> = memo(() => {
               `}
             >
               <Loader mode='default' />
-            </Wrapper>
+            </Elements.Wrapper>
           }
         >
           <MainHeader />
         </Suspense>
       </ErrorBoundary>
-      <Wrapper
+      <Elements.Wrapper
         customCss={css`
           display: flex;
           align-items: center;
@@ -110,148 +81,12 @@ const Component: React.FunctionComponent<{}> = memo(() => {
             }
           `}
         >
-          {/* <Carousel
-            index={currentIndex}
-            showControls={false}
-            showIndicators={false}
-          >
-            <Wrapper customCss={css`flex: 0 0 100%;`}>
-              <Layout.Title color='light' dangerouslySetInnerHTML={{__html: 'Reducir mi donación'}}/>
-              <Layout.Text color='light'>Para nosotros es muy importante que sigamos trabajando juntos en las causas más importantes</Layout.Text>
-              <UserDataForm />
-            </Wrapper>
-            <Wrapper customCss={css`flex: 0 0 100%;`}>
-              <Layout.Title color='light' dangerouslySetInnerHTML={{__html: 'Tu solicitud aún no termina.<br>Por favor, <em>mirá el video</em> antes de continuar'}}/>
-              <VideoPlayer videoUrl='https://www.youtube.com/watch?v=FXr3_zGc0O4' />
-            </Wrapper>
-            <Wrapper customCss={css`flex: 0 0 100%; background-color: orangered;`}>Element #3</Wrapper>
-          </Carousel> */}
           <ContentSlider />
         </Layout.Panel>
-      </Wrapper>
-      {/* <Layout.Panel>
-        <Layout.PanelWrapper
-          customCss={css`
-            padding-top: ${pixelToRem(35)};
-            padding-bottom: ${pixelToRem(35)};
-
-            @media (min-width: ${({ theme }) => pixelToRem(theme.responsive.tablet.minWidth)}) {
-              padding-top: ${pixelToRem(55)};
-              padding-bottom: ${pixelToRem(55)};
-            }
-          `}
-        >
-          <Layout.Title
-            color='primary'
-          >Lamentamos que tengas que cancelar tu donación</Layout.Title>
-          <Layout.Text>Comprendemos que necesites dejar de aportar, debe ser una decisión difícil<br/>para vos. Lo es también para nosotros por la importancia de tu ayuda.</Layout.Text>
-        </Layout.PanelWrapper>
-      </Layout.Panel>
-      <Layout.Panel 
-        customCss={css`
-          background-color: ${({ theme }) => theme.color.secondary.light};
-        `}
-      >
-        <Layout.PanelWrapper
-          customCss={css`
-            padding-top: ${pixelToRem(30)};
-
-            @media (min-width: ${({ theme }) => pixelToRem(theme.responsive.tablet.minWidth)}) {
-              padding-top: ${pixelToRem(55)};
-            }
-          `}
-        >
-          <Layout.Title>Continuaremos luchando por las causas que más nos necesitan</Layout.Title>
-          <Layout.Cards
-            customCss={css`
-              margin: ${pixelToRem(50)} 0 ${pixelToRem(80)};
-              justify-content: space-between !important;
-            `}
-          >
-            <Card
-              description='La agricultura es la principal causa de deforestación, por eso es necesario seguir generando conciencia.'
-              icon='sustentability'
-              title='Promover una vida sustentable'
-            />
-            <Card
-              description='6 de cada 7 especies de tortugas marinas están en peligro de extinción.'
-              icon='ocean'
-              title='Seguir protegiendo los océanos'
-            />
-            <Card
-              description='Los incendios en humedales que logramos visibilizar gracias a tu ayuda y tantas otras por las que seguiremos luchando cada día.'
-              icon='trees'
-              title='Protección de humedales'
-            />
-          </Layout.Cards>
-          <VideoPlayer videoUrl='https://www.youtube.com/watch?v=FXr3_zGc0O4' />
-        </Layout.PanelWrapper>
-      </Layout.Panel>
-      <Layout.Panel
-        customCss={css`
-          padding-bottom: ${pixelToRem(30)};
-          background-color: ${({ theme }) => theme.color.secondary.light};
-          
-          @media (min-width: ${({ theme }) => pixelToRem(theme.responsive.tablet.minWidth)}) {
-            padding-bottom: ${pixelToRem(55)};
-          }
-          `}
-      >
-        <Wrapper
-          customCss={css`
-            display: flex;
-            flex-direction: column;
-            margin-bottom: ${pixelToRem(50)} !important;
-          `}
-        >
-          <Layout.Title color='primary'>Antes de seguir, tomate un minuto para pensarlo.</Layout.Title>
-          <Layout.Text>Recordá que en Greenpeace:</Layout.Text>
-        </Wrapper>
-        <Layout.Cards>
-          <Card
-            title='No recibimos aportes de empresas privadas.'
-            icon='factory'
-          />
-          <Card
-            title='No recibimos aportes de partidos políticos ni estamos vinculados con ellos.'
-            icon='government'
-          />
-        </Layout.Cards>
-      </Layout.Panel>
-      <Layout.Panel>
-        <Wrapper
-          customCss={css`
-            @media (min-width: ${({theme}) => pixelToRem(theme.responsive.tablet.minWidth)}) {
-              max-width: ${pixelToRem(1140)};
-              align-self: center;
-              justify-content: space-between;
-            }
-          `}
-        >
-        <Layout.Title
-          color='primary'
-          customCss={css``}
-        >La decisión siempre está {mobile ? <br/> : null} en tus manos</Layout.Title>
-        <Layout.Text>Siempre podrás reducir el monto de tu donación o cancelarla directamente, sin vueltas. Si aún no estás seguro, podés hacerlo en otro momento.</Layout.Text>
-        </Wrapper>
-      </Layout.Panel>
-      <Layout.Panel
-        customCss={css`
-          padding-left: 0;
-          padding-right: 0;
-          padding-bottom: 0;
-        `}
-      >
-        <ErrorBoundary fallback='Form Error'>
-          <Suspense fallback={<Loader mode='default' />}>
-            <FormRouter />
-          </Suspense>
-        </ErrorBoundary>
-      </Layout.Panel> */}
-    </View>
+      </Elements.Wrapper>
+    </Elements.View>
   ), [
     mobile,
-    currentIndex,
   ]);
 });
 
