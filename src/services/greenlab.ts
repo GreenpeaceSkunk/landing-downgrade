7a5e5141351f8b68acdaf4387bed75a123d08862
Dan Tovbein <dtovbein@gmail.com>
2022-09-07 17:54:38 -0300

    Include greenlab services

---
+++ src/services/greenlab.ts
@@ -0,0 +1,54 @@
+import { ApiCall } from '../utils/apiCall';
+
+export const getApiUrl = (): string => {
+  return (`${process.env.REACT_APP_ENVIRONMENT}` === 'development')
+    ? `${process.env.REACT_APP_GREENLAB_API_URL}`
+    : `${window.location.origin}${process.env.REACT_APP_GREENLAB_API_URL}`;
+}
+
+export const updateContact = async (email: string, data: any) => {
+  try {
+    const response = await ApiCall({
+      headers: {
+        'X-Greenlab-App': `${window.sessionStorage.getItem('greenlab_app_name')}`,
+      },
+      baseURL: `${getApiUrl()}/hubspot/contact/email/${email}`,
+      method: 'POST',
+      data,
+    });
+    return response;
+  } catch(error: any) {
+    console.log(error);
+  }
+}
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const response = await ApiCall({
+      headers: {
+        'X-Greenlab-App': `${window.sessionStorage.getItem('greenlab_app_name')}`,
+      },
+      baseURL: `${getApiUrl()}/hubspot/contact/email/${email}`,
+      method: 'GET',
+    });
+    return response;
+  } catch(error: any) {
+    console.log(error);
+  }
+}
+
+export const getUserById = async (id: string) => {
+  console.log(getApiUrl());
+  try {
+    const response = await ApiCall({
+      headers: {
+        'X-Greenlab-App': `${window.sessionStorage.getItem('greenlab_app_name')}`,
+      },
+      baseURL: `${getApiUrl()}/hubspot/contact/id/${id}`,
+      method: 'GET',
+    });
+    return response;
+  } catch(error: any) {
+    console.log(error);
+  }
+}
